3052404050d9dabe57fa325d426b94f228380625
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-04-30 14:11:12 +0900

    条件分岐

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // alert('Hello World');
 
-let hello = 'Hello World';
-alert(hello);
+// let hello = 'Hello World';
+// alert(hello);
 
-alert(3 + 4);
-alert(5 - 1);
-alert(7 * 2);
-alert(9 / 3);
+// alert(3 + 4);
+// alert(5 - 1);
+// alert(7 * 2);
+// alert(9 / 3);
 
-alert('Hello' + 'World');
+// alert('Hello' + 'World');
+
+let orange = 120;
+let apple = 130;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange = apple){
+  alert('みかんとりんごは同じ値段');
+} else{
+  alert('みかんの値段はりんごより高い');
+}
+

14220fa21c555c13558f5b24b8a7e387aac3926f
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-04-30 18:17:57 +0900

    [Add]ホイスティングの確認

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -68,3 +68,22 @@
 
 // console.log(i);
 
+
+
+// 変数の巻き上げ（ホイスティング）
+var str = "webcamp";
+
+function foo(){
+  console.log(str);
+  var str = "dmm webcamp";
+  console.log(str);
+}
+//上記と下記のコードは同じ
+function foo(){
+  var str
+  console.log(str);
+  str = "dmm webcamp";
+  console.log(str);
+}
+
+foo()

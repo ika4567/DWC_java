1dd54556a79bb09648768c6bc98cd3597926ffab
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-04-30 18:02:31 +0900

    [Add]スコープの確認

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -30,7 +30,41 @@
 // console.log(nickname);
 
 // constによる再代入→エラーになる
-const nickname = "taro";
-console.log(nickname);
-nickname = "jiro";
-console.log(nickname);
+// const nickname = "taro";
+// console.log(nickname);
+// nickname = "jiro";
+// console.log(nickname);
+
+
+
+//varのスコープ
+// var str = "webcamp"; // グローバルスコープ
+
+// function foo(){
+//   console.log(str);
+//   var y = "hello"; //関数スコープ
+// }
+
+// foo()
+// console.log(y);
+
+
+//letのスコープ(ローカルスコープ)
+// function foo(){
+//   let x = "webcamp";
+//   {
+//     let y = "hello webcamp";
+//   }
+//   console.log(x);
+//   console.log(y);
+// }
+
+// foo()
+
+
+// for(let i = 0; i < 10; i++){
+//   console.log(i);
+// }
+
+// console.log(i);
+

ec693e2605caecca5c11d1f3b77b8ac91362bdb4
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-01 16:31:58 +0900

    [Add]3章 関数

--- app.js
+++ app.js
@@ -64,7 +64,85 @@
 
 
 // 確認問題
-alert(20 + 2);
-alert(20 - 2);
-alert(20 * 2);
-alert(20 / 2);
+// alert(20 + 2);
+// alert(20 - 2);
+// alert(20 * 2);
+// alert(20 / 2);
+
+
+
+// 関数を呼び出す
+// let alertString;
+// alertString = addString("Webcamp");
+
+// alert(alertString);
+
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+
+// // 入力ダイアログで値を入力する
+// let promptStr = prompt('何か好きな文字を入力してください');
+
+// alert(promptStr);
+
+
+// 複数の関数を定義（入力した値を処理する関数を作成）
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+
+let js_hand = getJShand(); //じゃんけんの手をランダムに生成する関数を呼び出す
+
+let judge = winlose(user_hand, js_hand); //ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出す
+
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winlose(user, js){
+  let winloseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winloseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winloseStr = "勝ち";
+    } else if(js == "パー"){
+      winloseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winloseStr = "負け";
+    } else if(js == "チョキ"){
+      winloseStr = "あいこ";
+    } else if(js == "パー"){
+      winloseStr = "勝ち";
+    }
+  } else if(usre == "パー"){
+    if(js == "グー"){
+      winloseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winloseStr = "負け";
+    } else if(js == "パー"){
+      winloseStr = "あいこ";
+    }
+  }
+
+  return winloseStr;
+}

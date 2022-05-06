cc245af0b593e85c9b8676b080dcbc9cd7ab767b
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-06 12:11:14 +0900

    [Add]確認問題

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,21 +1,32 @@
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!!</h1>');
+// // $(document).ready(function () {
+// //   $('body').html('<h1>Hello jQuery!!</h1>');
+// // });
+
+// $(function(){
+//   // $('.box1').css({
+//   //   'background-color': '#0000FF', //一つのCSSを記述するときは $('セレクタ').css('プロパティ名','値');
+//   //   'height': '100px'
+//   // });
+
+//   // $('.box1').slideDown(); //上から下へスライド(無から要素を表示)させるアニメーション
+
+//   // $('.box1').slideUp(); //下から上へスライドさせる(表示されてる要素を隠す)アニメーション
+
+//   // 非表示の要素を表示させる
+//   // $('.box1').show();
+//   // $('.box1').css('background-color','#0000FF');
+
+//   // 表示されている要素を非表示にする
+//   // $('.box1').hide();
 // });
 
+// 確認問題
 $(function(){
-  // $('.box1').css({
-  //   'background-color': '#0000FF', //一つのCSSを記述するときは $('セレクタ').css('プロパティ名','値');
-  //   'height': '100px'
-  // });
-
-  // $('.box1').slideDown(); //上から下へスライド(無から要素を表示)させるアニメーション
-
-  // $('.box1').slideUp(); //下から上へスライドさせる(表示されてる要素を隠す)アニメーション
-
-  // 非表示の要素を表示させる
-  // $('.box1').show();
-  // $('.box1').css('background-color','#0000FF');
-
-  // 表示されている要素を非表示にする
-  $('.box1').hide();
+  $('.box1').slideDown();
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px',
+    'width': '200px'
+  });
+  $('.box1').slideUp();
 });

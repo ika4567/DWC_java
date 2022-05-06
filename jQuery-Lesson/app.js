ba2a21838c0224c60b7c747240e49d9946e56ad5
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-06 11:43:49 +0900

    [Add]jQueryでdiv要素を変更する

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,10 @@
 // $(document).ready(function () {
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
+
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF', //一つのCSSを記述するときは $('セレクタ').css('プロパティ名','値');
+    'height': '100px'
+  });
+});

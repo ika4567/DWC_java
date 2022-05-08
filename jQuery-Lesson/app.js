ec73cea804091aa2ce276dc7a70f098934794154
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-08 12:17:31 +0900

    [Add]マウスオーバー､アウトをclass属性で書き換え

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -35,9 +35,11 @@
 // イベント検出をする
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color':'#0000FF'});
+    // $('.box1').css({'background-color':'#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color':'#FF0000'});
+    // $('.box1').css({'background-color':'#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });

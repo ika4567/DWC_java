1c3fe14bf185463916d479a4789dbe66facf04cc
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-10 14:07:56 +0900

    [Add]thisの使い方を学ぶ

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -52,3 +52,24 @@
 //     $('.box1').removeClass('box1-ext');
 //   });
 // });
+
+
+
+$(function(){
+  $('.bg1').on('click', function(){
+    // $('.bg1').slideUp();
+    $(this).slideUp(); //thisで書き換え
+  });
+  
+  $('.bg2').on('click', function(){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function(){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function(){
+    $('.bg4').slideUp();
+  });
+});

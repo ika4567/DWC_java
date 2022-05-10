27dcbbe6475ab61c27560514f6b2ad8c8b7ac253
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-10 16:09:26 +0900

    [Add]childrenの使い方を学ぶ

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -55,21 +55,27 @@
 
 
 
-$(function(){
-  $('.bg1').on('click', function(){
-    // $('.bg1').slideUp();
-    $(this).slideUp(); //thisで書き換え
-  });
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     // $('.bg1').slideUp();
+//     $(this).slideUp(); //thisで書き換え
+//   });
   
-  $('.bg2').on('click', function(){
-    $('.bg2').slideUp();
-  });
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
 
-  $('.bg3').on('click', function(){
-    $('.bg3').slideUp();
-  });
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
 
-  $('.bg4').on('click', function(){
-    $('.bg4').slideUp();
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });

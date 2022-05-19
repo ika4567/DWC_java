ac36afba015b52bf846a6b3427e2643bb1fbca4c
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-19 23:56:05 +0900

    [Add]jQueryでタブをクリック時のイベントを設定

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,7 +1,17 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active'); //.toggleClass()で引数に指定されているclassが存在する場合にはそのclassが削除され、無い場合には追加される
-    $('#sp-menu').fadeToggle(); //.fadeToggle()は要素のフェードインとフェードアウトを切り替えるメソッド
-    event.preventDefault();
-  });
+// $(function() {
+//   $('.menu-trigger').on('click', function(event) {
+//     $(this).toggleClass('active'); //.toggleClass()で引数に指定されているclassが存在する場合にはそのclassが削除され、無い場合には追加される
+//     $('#sp-menu').fadeToggle(); //.fadeToggle()は要素のフェードインとフェードアウトを切り替えるメソッド
+//     event.preventDefault();
+//   });
+// });
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });

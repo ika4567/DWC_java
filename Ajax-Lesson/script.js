552a9c600df387ba36be6513d4d3765924f7070c
ika4567 <97078291+ika4567@users.noreply.github.com>
2022-05-20 11:29:44 +0900

    [Add]Ajaxの実装

--- Ajax-Lesson/script.js
+++ Ajax-Lesson/script.js
@@ -1,17 +1,10 @@
-// $(function() {
-//   $('.menu-trigger').on('click', function(event) {
-//     $(this).toggleClass('active'); //.toggleClass()で引数に指定されているclassが存在する場合にはそのclassが削除され、無い場合には追加される
-//     $('#sp-menu').fadeToggle(); //.fadeToggle()は要素のフェードインとフェードアウトを切り替えるメソッド
-//     event.preventDefault();
-//   });
-// });
+const API_KEY = "11a2718bf5cdf9c6b6baad56e8517a74"
 
-$('#tab-contents .tab[id != "tab1"]').hide();
-
-$('#tab-menu a').on('click', function(event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    })
+  });
 });
